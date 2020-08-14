e02f19edf6194ee90eccfd00acfaf5da4c87a4fb
jorgevz <jorgev2132@gmail.com>
2020-08-14 18:32:31 -0400

    div alignment

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,14 +3,19 @@ import React from 'react';
 function Home(){
 return(
 <div>
+
 <section className="nav-logo-welcome">
 <img src="./mylogo.jpeg" alt='dogfo-logo'/>
 <h1>Welcome to our site ! where Dog Education matters</h1>
+
 </section>
 //
-<div>
+<section>
+
 <p></p>
-</div>
+
+</section>
+
 </div>
 );
 }
