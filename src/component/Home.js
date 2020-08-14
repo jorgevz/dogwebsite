4c8760b352726de198e1b8222891162bc588ecfd
jorgevz <jorgev2132@gmail.com>
2020-08-14 17:24:04 -0400

    class added for section

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home(){
 return(
-<div>
+<section className="nav-logo-welcome">
 <img src="./mylogo.jpeg" alt='dogfo-logo'/>
 <h1>Welcome to our site ! where Dog Education matters</h1>
 
