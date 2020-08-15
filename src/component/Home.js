f8186e6958b3fb6a328a1111381e0625d9d15978
jorgevz <jorgev2132@gmail.com>
2020-08-14 22:37:23 -0400

    second div

--- src/component/Home.js
+++ src/component/Home.js
@@ -18,7 +18,7 @@ that is why we make sure we have the right content to share with you all. We pro
 information about all dog breeds for new and experienced dog owners </p>
 </div>
 
-<div>
+<div id='second-article'>
 <h1>You find yourself bored ? come to us</h1>
 <p>Other than the fact that we educate we want to say that we also entertain since the content we pro </p>
 </div>
