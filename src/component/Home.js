49c1ef9e4f7b1d547998945a1c75b2c6dba23cac
jorgevz <jorgev2132@gmail.com>
2020-08-14 20:17:30 -0400

    paragraph implement

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,8 +10,10 @@ return(
 <img src={logo} alt='dogfo-logo' width='150px' height='150px'/>
 </section>
 
-<article>
+<article className='first-article'>
 <h1>Welcome to our site, where Dog Education matters</h1>
+<p>We pride ourselves in the type of knowledge we provide the dog lovers community
+that is why we make sure we have the right content to share with you all. </p>
 </article>
 
 <section>
