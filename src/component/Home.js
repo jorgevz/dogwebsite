87543f22d6e4398ff3d3c7454c4267f41f37223c
jorgevz <jorgev2132@gmail.com>
2020-08-14 21:21:30 -0400

    main section created

--- src/component/Home.js
+++ src/component/Home.js
@@ -9,9 +9,9 @@ return(
 <img src={logo} alt='dogfo-logo' width='150px' height='100px'/>
 </section>
 
-<section>
+<section className='main-article'>
 
-<div className='first-article'>
+<div id='first-article'>
 <h1>Welcome to our site, where Dog Education matters</h1>
 <p>We pride ourselves in the type of knowledge we provide to the dog lovers community,
 that is why we make sure we have the right content to share with you all. </p>
