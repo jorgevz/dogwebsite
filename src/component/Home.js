74e1049569237e87c53890af350eb939677ae3c2
jorgevz <jorgev2132@gmail.com>
2020-08-15 22:20:01 -0400

    overall paragraph

--- src/component/Home.js
+++ src/component/Home.js
@@ -15,12 +15,13 @@ return(
 <h1>Welcome to our site, where Dog Education matters</h1>
 <p>We pride ourselves in the type of knowledge we provide to the dog lovers community,
 that is why we make sure we have the right content to share with you all. We provide 
-information about all dog breeds for new and experienced dog owners </p>
+information about all dog breeds for new and experienced dog owners. </p>
 </div>
 
 <div id='second-article'>
 <h1>You find yourself bored ? come to us</h1>
-<p>Other than the fact that we educate we want to say that we also entertain since the content we pro </p>
+<p>Other than the fact that we educate we want to say that we also entertain since the content we provide
+is very entertaining to those who find dog education entertaining. </p>
 </div>
 
 </section>
