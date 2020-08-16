5d211da70806349fd2ef33ad89b291e77cc22913
jorgevz <jorgev2132@gmail.com>
2020-08-15 22:59:26 -0400

    dogpicimplemented

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './Background.svg'
+import dogimage from './dogfopic.png'
 
 function Home(){
 return(
@@ -17,6 +18,10 @@ return(
 that is why we make sure we have the right content to share with you all. We provide 
 information about all dog breeds for new and experienced dog owners. </p>
 </div>
+
+<section>
+<img src={dogimage} height='150px' width='100px'/>
+</section>
 
 <div id='second-article'>
 <h1>We also entertain</h1>
