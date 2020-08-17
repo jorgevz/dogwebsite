0cb47c72a3bf11e9b0a021c9b9d43f9113ccf875
jorgevz <jorgev2132@gmail.com>
2020-08-16 22:50:30 -0400

    new animation created

--- src/component/Mission.js
+++ src/component/Mission.js
@@ -10,6 +10,8 @@ return(
 </section>
 
 <div className='mission-statement'>
+
+<div id='mission-inside'>
 <h1>So what is our mission ?</h1>
 <p>We want to be able to provide the best dog education & dog training. 
 We are on a mission not only to train your dog but to also give you the best dog education, this is
@@ -17,6 +19,7 @@ good for all of us and by that we mean that not everyone is a dog geek but that 
 who would like to understand their dogs better according to their breed. 
 </p>
 <img id='pic-1' src='https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/2190959/691786_62694.jpeg' />
+</div>
 
 
 </div>
