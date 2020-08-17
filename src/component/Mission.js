5f501892fc6e082a0424d4040f66f990acb38c40
jorgevz <jorgev2132@gmail.com>
2020-08-16 21:44:22 -0400

    picture added

--- src/component/Mission.js
+++ src/component/Mission.js
@@ -16,7 +16,12 @@ We are on a mission not only to train your dog but to also give you the best dog
 good for all of us and by that we mean that not everyone is a dog geek but that there also clients
 who would like to understand their dogs better according to their breed. 
 </p>
+<img id='pic-1' src='https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/2190959/691786_62694.jpeg' />
+
+
 </div>
+
+
 
 
 
