d8fc116d5a01a19884472f83b35537ac74f6bfa3
jorgevz <jorgev2132@gmail.com>
2020-08-16 20:44:24 -0400

    more editing

--- src/component/Mission.js
+++ src/component/Mission.js
@@ -9,16 +9,16 @@ return(
 <img src={logo} alt='dogfo-logo' width='150px' height='100px'/>
 </section>
 
-<h1 id='our-mission'>So what is our mission ?</h1>
-<p id='mission-statement'>We want to be able to provide the best dog education & dog training. 
+<div className='mission-statement'>
+<h1>So what is our mission ?</h1>
+<p>We want to be able to provide the best dog education & dog training. 
 We are on a mission not only to train your dog but to also give you the best dog education, this is
 good for all of us and by that we mean that not everyone is a dog geek but that there also clients
 who would like to understand their dogs better according to their breed. 
 </p>
+</div>
 
-<section>
 
-</section>
 
 </div>
 )
