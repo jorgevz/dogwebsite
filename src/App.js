47465b112816fbc5764369026307ca87586e2555
jorgevz <jorgev2132@gmail.com>
2020-08-18 23:15:00 -0400

    updates on new link

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
 import Form from './component/Form';
 import Dogs from './component/Dogs';
 import Services from './component/Services'
+import Location from './component/Locations'
+import Locations from './component/Locations';
   
   
 
@@ -26,6 +28,7 @@ function App() {
       <Link id='link-breeds' to='/Breeds'>Breeds</Link>
       <Link id='form-link' to='/Form'>Form</Link>
       <Link id='btn-services' to='/Services'>Services</Link>
+      <Link id='link-locations' to='/Locations'>Locations</Link>
 
       </nav>
       
@@ -36,6 +39,7 @@ function App() {
       <Route path='/Breeds' component={Dogs}/>
       <Route path='/Form' component={Form}/>
       <Route path='/Services' component={Services}/>
+      <Route path='/Locations' component={Locations}/>
      
       <Redirect path='/'/>
       </Switch>
--- src/component/Locations.js
+++ src/component/Locations.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React from 'react';
+import logo from './Background.svg';
+import charlotte from '../images/charlotte.jpg';
+import nashville from '../images/nashville.jpg';
+import chicago from '../images/chicago.jpg';
+import nyc from '../images/nyc.jpg'
+
 
 function Locations(){
 return(
 <div>
+ <img src={logo}/>
+<div className='cities'>
 
+
+
+<div id='city-one'>
+<h2>Chicago,IL</h2>
+<p></p>
+<img className='dog-images' src={chicago} height='500px' width='300px'/>
+</div>
+
+<div id='city-two'>
+<h2>Charlotte,NC</h2>
+<p></p>
+<img className='dog-images' src={charlotte} height='500px' width='300px%'/>
+</div>
+
+<div id='city-three'>
+<h2>Nashville,TN</h2>
+<p></p>
+<img className='dog-images' src={nashville} height='500px' width='300px'/>
+</div>
+
+<div id='city-three'>
+<h2>New York City</h2>
+<p></p>
+<img className='dog-images' src={nyc} height='500px' width='300px'/>
+</div>
 
 </div>
 
+</div>
 )
 }
 
