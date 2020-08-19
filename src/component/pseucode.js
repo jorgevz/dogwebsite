e412c7bbc9924dc6c78846493b0ad5bfac8e1b47
jorgevz <jorgev2132@gmail.com>
2020-08-19 00:21:13 -0400

    my submitted pseucode

---
+++ src/component/pseucode.js
@@ -0,0 +1,10 @@
+//As one may see my page starts at the 'Home' link here the user is able to see the most essential services 
+//we offer as a company as well as a welcoming page where the user clearly sees is a dog focused site. I used
+//Animation in this link to give my website a more appealing look for when the user enters it the first time.
+//for my mission's link i have rendering what is my company's mission.
+//In my breeds page you can see the API extracted where i specify each breed for its name, behavior etc..
+//In my form that is where we asked for the user to input their and their dog's information to get back to them
+//also this information is being stored live in our firebase database.
+//In my services link i have a detailed list of all our services we provide to dogowners.
+//The location's link where we clearly state where we have our franchises in the US.
+//Last but not least our contact link where we point out our different contact ways and departments.
